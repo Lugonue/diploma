f0030c7646f1c34097842172df76ee34cd89cd6b
dina lapteva <dikaamos@gmail.com>
2025-04-03 02:25:36 +0900

    delete imports in user.mo

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -3,8 +3,6 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Address } from './entities/address.entity';
-import { Phone } from './entities/phone.entity';
 
 @Module({
   controllers: [UserController],
