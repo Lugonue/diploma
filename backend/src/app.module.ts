0ccf075478bedb779d917e206aceb3d5ea8fc972
dina lapteva <dikaamos@gmail.com>
2025-04-03 14:38:15 +0900

    change path to env file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,7 +8,7 @@ import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '../.env.dev.local' }),
+    ConfigModule.forRoot({ envFilePath: ['.env.dev.local'] }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
@@ -21,7 +21,7 @@ import { DataSource } from 'typeorm';
         database: config.get('DB_DATABASE'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: config.get('DB_SYNCHRONIZE'), // false в production!
-        logging: true, // Логирование запросов (опционально)
+        logging: ['error', 'warn'], // Логирование запросов (опционально)
         autoLoadEntities: true,
       }),
       dataSourceFactory: async (options) => {
