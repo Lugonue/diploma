d76e5eece98ae4036d861fc0532eef25277a3813
dina lapteva <dikaamos@gmail.com>
2025-04-29 14:41:58 +0900

    add profile endpoint

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcryptjs';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -26,16 +27,27 @@ export class UserService {
     return await this.dataSource.getRepository(User).findOne({ where: { id }, relations: ['addresses', 'phones', 'orders'] });
   }
 
-  async update(id: number, updateUserDto: Partial<CreateUserDto>) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     await this.dataSource.getRepository(User).update(id, updateUserDto);
-    return await this.findOne(id);
+    return await this.getPublicUser(id);
   }
 
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
       throw new NotFoundException(`User with id ${id} not found`);
-  }
+    }
     return await this.dataSource.getRepository(User).remove(user);
+  }
+
+  async getPublicUser(id: number) {
+    const user = await this.dataSource.getRepository(User).findOne({ where: { id }, relations: ['addresses', 'phones', 'orders'] });
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    const publicUser = Object.fromEntries(
+      Object.entries(user).filter(([key]) => key !== 'password')
+    );
+    return publicUser;
   }
 }
