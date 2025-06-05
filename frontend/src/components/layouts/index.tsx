8697969705d821c9d75e0d609f04e00d49ec0ab8
PopkoKV <lugonue@ya.ru>
2025-06-05 10:46:58 +0300

    F: fix logo, fix cdtalog

--- frontend/src/components/layouts/index.tsx
+++ frontend/src/components/layouts/index.tsx
@@ -1,30 +1,29 @@
-import Header from "components/template/Header"
-import { Toaster } from "components/ui/sonner"
-import useUserStore from "hooks/stores/useUserStore"
-import { useEffect } from "react"
-import { Outlet, useNavigate } from "react-router"
+import Header from "components/template/Header";
+import { Toaster } from "components/ui/sonner";
+import useUserStore from "hooks/stores/useUserStore";
+import { Outlet, useNavigate } from "react-router";
 
-type Props = {}
+type Props = {};
 
 const Layout = (props: Props) => {
-    const { user } = useUserStore();
-    const navigate = useNavigate()
+  const { user } = useUserStore();
+  const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     if (!user.hasAuth) {
-    //         navigate('/auth/login')
-    //     }
-    // }, [])
+  // useEffect(() => {
+  //     if (!user.hasAuth) {
+  //         navigate('/auth/login')
+  //     }
+  // }, [])
 
-    return (
-        <>
-            <main className="min-h-screen flex flex-col bg-emerald-100 overflow-hidden">
-                <Header id="header" />
-                <Outlet />
-                <Toaster position="top-center" />
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <main className="min-h-screen flex flex-col bg-emerald-100 overflow-hidden pt-[5rem]">
+        <Header id="header" />
+        <Outlet />
+        <Toaster position="top-center" />
+      </main>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- frontend/src/components/template/blocks/Catalog/CatalogElement.tsx
+++ frontend/src/components/template/blocks/Catalog/CatalogElement.tsx
@@ -1,22 +1,28 @@
-import { Card, CardContent } from 'components/ui/card'
-import { Category } from 'hooks/stores/useCatalogStore'
-import useProductStore from 'hooks/stores/useProductStor'
-import React from 'react'
-import { useNavigate } from 'react-router'
-
-
+import { Card, CardContent } from "components/ui/card";
+import useCatalogStore, { Category } from "hooks/stores/useCatalogStore";
+import useProductStore from "hooks/stores/useProductStor";
+import { useNavigate } from "react-router";
 
 const CatalogElement = ({ name, id }: Category) => {
-    const { setRequestProductParams, requestProductParams } = useProductStore()
-    const navigator = useNavigate()
-    return (
-        <Card className='hover:scale-105 transition-transform cursor-pointer'>
-            <CardContent className="flex flex-col gap-3 lg:w-[200px] min-h-[200px] " onClick={() => { setRequestProductParams({ ...requestProductParams, categoryId: id }); navigator(`/catalog`) }}>
-                <span className="text-l font-bold p-2 bg-secondary rounded text-center">{name}</span>
-                {/* <p className='text-sm text-gray-400'>{}</p> */}
-            </CardContent>
-        </Card >
-    )
-}
+  const { setCurrentCategoryId } = useCatalogStore();
+  const { setRequestProductParams, requestProductParams } = useProductStore();
+  const navigator = useNavigate();
+  return (
+    <Card className="hover:scale-105 transition-transform cursor-pointer">
+      <CardContent
+        className="flex flex-col gap-3 lg:w-[200px] min-h-[200px] "
+        onClick={() => {
+          setRequestProductParams({ ...requestProductParams, categoryId: id });
+          setCurrentCategoryId(id);
+          navigator(`/catalog`);
+        }}
+      >
+        <span className="text-l font-bold p-2 bg-secondary rounded text-center">
+          {name}
+        </span>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default CatalogElement
+export default CatalogElement;
